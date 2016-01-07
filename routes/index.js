e85c1258b4ada5d86faa1de62c9e4fd18c1a84e2
HathePhoby <ketro156@gmail.com>
2016-01-07 20:55:46 +0100

    Add New flat Working

--- routes/index.js
+++ routes/index.js
@@ -29,5 +29,45 @@ router.get('/', function(req, res) {
     });
   });
 
+router.get('/newflat', function(req, res) {
+    res.render('newflat', { title: 'Add New Flat' });
+});
+
+/* POST to Add User Service */
+router.post('/addFlat', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var name = req.body.flatFlatName;
+    var city = req.body.flatCity;
+    var address = req.body.flatAddress;
+    var floor = req.body.flatFloor;
+    var numberOfRooms = req.body.flatNumberOfRooms;
+    var postCode = req.body.flatPostCode;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "Flat name" : name,
+        "city" : city,
+        "address" : address,
+        "floor" : floor,
+        "number of rooms" : numberOfRooms,
+        "post code" : postCode
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
 
 module.exports = router;
